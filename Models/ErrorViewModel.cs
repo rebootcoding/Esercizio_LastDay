cc0848ba9da2d3e9476466c58889ae2b40d3d006
Michele Francesco Meloni <michele.f.meloni@avanade.com>
2023-04-27 09:29:59 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esercizio_LastDay.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
